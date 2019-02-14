3541299fe7217be075620e65804031fdfb56cefe
roman <rashamba323@gmail.com>
2019-02-14 18:49:10 +0300

    Add main methods

--- app/src/main/java/com/example/itapp/MainActivity.java
+++ app/src/main/java/com/example/itapp/MainActivity.java
@@ -10,4 +10,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    Utils utils = new Utils();
+
+
+
+    private void init(){
+        utils.create("Затраты на обучение ИТ-персонала в год", 1000, "P");
+        utils.create("Средние затраты на закупку оборудования в год", 31600, "K");
+        utils.create("Консультационные услуги третьих фирм и другие затраты на обслуживание", 6000, "K");
+        utils.create("Средние затраты на программное обеспечение в год", 12000, "P");
+        utils.create("Ежегодные затраты на комплектующие", 24000, "K");
+        utils.create("Стоимость обслуживания техники по контрактам", 600, "K");
+    }
+
+    private void run(){
+        utils.print();
+        utils.yearCost(utils.pcostList);
+    }
 }
+
